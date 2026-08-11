4ace7a4dda327e469844b098b670b03edcd642cf
Rishi Rithesh <rishi2470003@ssn.edu.in>
2026-08-11 19:16:39 +0530

    Update text from 'Doors close in' to 'Registration closes in'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -121,7 +121,7 @@ export default function Home() {
                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-accent-400 opacity-75"></span>
                     <span className="relative inline-flex rounded-full h-2 w-2 bg-accent-500"></span>
                   </span>
-                  Doors close in
+                  Registration closes in
                 </p>
                 <div className="relative z-10">
                   <Countdown />
